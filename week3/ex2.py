e6a4f08b9fc3e1f49a6cee827ea2447669160993
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2018-03-20 15:59:56 -0300

    Make ex2 run correctly on simple test case

--- week3/ex2.py
+++ week3/ex2.py
@@ -1,21 +1,20 @@
-
-
-def recursive_solve(answers, nums, current, pos, count, total):
+def recursive_solve(nums, current, pos, count, total):
     if count == total:
-        answers.append(current)
+        global answers
+        answers.add(tuple(current))
         return
     if count > total:
         return
-    
     size = len(nums)
     i = pos
     while i < size:
-        recursive_solve(answers, nums, current + [nums[i]], pos+1, count+nums[i], total)
-        for j in range(pos+1, size):
+        recursive_solve(nums, current + [nums[i]], i+1, count+nums[i], total)
+        j = i + 1
+        while j < size:
             if nums[j] != nums[pos]:
                 break
+            j += 1
         i = j
-
 
 
 def read_input():
@@ -23,17 +22,14 @@ def read_input():
     return line[0], line[1], line[2:]
 
 def main():
-    t, n , nums = read_input()
-    answers = []
+    total, n, nums = read_input()
+    global answers
+    answers = set()
     if n == 0:
         exit()
-   
-    recursive_solve(answers, nums, [], 0, 0, t)
-    print(answers)
+    recursive_solve(nums, [], 0, 0, total)
+    print("anserws: ", answers)
 
 
 if __name__ == "__main__":
     main()
-
-
-
