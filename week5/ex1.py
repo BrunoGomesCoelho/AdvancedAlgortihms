a203782c46ff5a3b415de0889afc7769857ddc04
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2018-04-09 17:25:45 -0300

    Start week5

---
+++ week5/ex1.py
@@ -0,0 +1,13 @@
+PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+          43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+def factorial_primes(n):
+    pass
+
+def main():
+    n = int(input())
+    while n != 0:
+        factorial_primes(n)
+
+if __name__ == "__main__":
+    main()
