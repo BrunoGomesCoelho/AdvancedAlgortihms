2874456e89b6dc35ab5a03c2e6ad803ec801d76c
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2018-04-12 14:13:25 -0300

    Finish exercise 3 and 4

--- week5/ex3.py
+++ week5/ex3.py
@@ -1,5 +1,23 @@
+import sys
+
+PRIME = 131071
+
 def main():
-    pass
+    char = sys.stdin.read(1)
+    x = 0
+    while char:
+        if char == "\n":
+            char = sys.stdin.read(1)
+        elif char == "#":
+            if x == 0:
+                print("YES")
+            else:
+                print("NO")
+            x = 0
+            char = sys.stdin.read(1)
+        else:
+            x  = (2*x + int(char)) % PRIME
+            char = sys.stdin.read(1)
 
 if __name__ == "__main__":
     main()
--- week5/ex4.py
+++ week5/ex4.py
@@ -1,5 +1,15 @@
+import sys
 def main():
-    pass
+    line = [int(x) for x in sys.stdin.readline().split()[:-1]]
+    while line:
+        num_biggest = max(line)
+        mod_biggest = 1
+        for i in range(2, num_biggest):
+            if all(num % i == line[0] % i for num in line):
+                mod_biggest = i
+        print(mod_biggest)
+        
+        line = [int(x) for x in sys.stdin.readline().split()[:-1]]
 
 if __name__ == "__main__":
     main()
