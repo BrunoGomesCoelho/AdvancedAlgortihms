4d8636ba153d800cb3c62a35b7d05d6d4f55c817
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2018-06-25 16:40:49 -0300

    Start working on ex3 (TSP with DP)

---
+++ week10/ex3.py
@@ -0,0 +1,15 @@
+
+def main():
+    num_cases = int(input())
+    for _ in range(num_cases):
+        x, y = map(int, input().split())
+        size = max(x, y)
+        MEMO = [[-1]*(2**size) for _ in range(size)]
+
+        start_x, end_x = map(int, input().split() 
+        num_points = int(input())
+        distances = []
+
+
+if __name__ == "__main__":
+    main()
